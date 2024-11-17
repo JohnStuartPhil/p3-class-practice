6b68a4d1254c58784a2754b782bf44a593aa6c9c
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-17 22:46:20 +0000

    end of day

--- run.py
+++ run.py
@@ -11,15 +11,95 @@ class Countries:
     
     def question(self):
         """the question"""
-        return f"What is the capital of {self.country}?\nIs it {self.a}: {self.incorrect_1}, {self.b}: {self.incorrect_2} or {self.c}: {self.capital}"
+        return f"What is the capital of {self.country}?\nIs it; {self.a}: {self.incorrect_1}, {self.b}: {self.incorrect_2} or {self.c}: {self.capital}"
 
-question_1 = Countries("United States", "New York", "Los Angeles", "Washington DC", "A", "B", "C")
+question_1 = Countries("the United States", "New York", "Los Angeles", "Washington DC", "A", "B", "C")
 print()
 print(question_1.question())
+print()
+def determine_if_b_is_the_corrrect_answer():
+    """
+    determines the output once the user has input A/a, B/b, C/c or something else
+    """
+    answer = input("Please select an option of A, B or C: ")
+    print()
+    print(f"You selected {answer}")
+    print()
+
+    if answer.upper() == "B":
+        print(f"Well done, {answer} is the correct answer")
+        print()
+    elif answer.upper() == "A":
+        print(f"{answer} is not the correct answer")
+        print()
+    elif answer.upper() == "C":
+        print(f"{answer} is not the correct answer")    
+        print()
+    else:
+        print(f"{answer} is not an option, please try again and choose an option of A, B or C")
+        print()
+        the_question()
+        print()
+        the_options()
+        print()
+determine_if_b_is_the_corrrect_answer()
+
 question_2 = Countries("China", "Beijing", "Hong Kong", "Shanghai", "A", "B", "C")
-print()
 print(question_2.question())
-question_3 = Countries("Germany", "Berlin", "Hamburg", "Munich", "A", "B", "C")
 print()
+def determine_if_c_is_the_corrrect_answer():
+    """
+    determines the output once the user has input A/a, B/b, C/c or something else
+    """
+    answer = input("Please select an option of A, B or C: ")
+    print()
+    print(f"You selected {answer}")
+    print()
+
+    if answer.upper() == "C":
+        print(f"Well done, {answer} is the correct answer")
+        print()
+    elif answer.upper() == "A":
+        print(f"{answer} is not the correct answer")
+        print()
+    elif answer.upper() == "B":
+        print(f"{answer} is not the correct answer")    
+        print()
+    else:
+        print(f"{answer} is not an option, please try again and choose an option of A, B or C")
+        print()
+        the_question()
+        print()
+        the_options()
+        print()
+determine_if_c_is_the_corrrect_answer()
+
+question_3 = Countries("the United Kingdom", "Birmingham", "London", "Manchester", "A", "B", "C")
 print(question_3.question())
 print()
+def determine_if_b_is_the_corrrect_answer():
+    """
+    determines the output once the user has input A/a, B/b, C/c or something else
+    """
+    answer = input("Please select an option of A, B or C: ")
+    print()
+    print(f"You selected {answer}")
+    print()
+
+    if answer.upper() == "B":
+        print(f"Well done, {answer} is the correct answer")
+        print()
+    elif answer.upper() == "A":
+        print(f"{answer} is not the correct answer")
+        print()
+    elif answer.upper() == "C":
+        print(f"{answer} is not the correct answer")    
+        print()
+    else:
+        print(f"{answer} is not an option, please try again and choose an option of A, B or C")
+        print()
+        the_question()
+        print()
+        the_options()
+        print()
+determine_if_b_is_the_corrrect_answer()
