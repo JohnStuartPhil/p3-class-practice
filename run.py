68ab4ad87ffdedd431c350d438e7167f7b5cdc27
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-11-19 16:14:50 +0000

    test36

--- run.py
+++ run.py
@@ -24,12 +24,12 @@ class Countries:
 question_1 = Countries("the United States", "Washington DC", "Los Angeles", "New York", "A", "B", "C")
 question_2 = Countries("China", "Beijing", "Hong Kong", "Shanghai", "A", "B", "C")
 question_3 = Countries("Germany", "Berlin", "Hamburg", "Munich", "A", "B", "C")
-question_4 = Countries("Japan", "Hiroshima", "Osaka", "Toyko", "A", "B", "C")
-question_5 = Countries("India", "Chennai", "Mumbai", "New Deheli", "A", "B", "C")
-question_6 = Countries("the United Kingdom", "Birmingham", "London", "Manchester", "A", "B", "C")
-question_7 = Countries("France", "Lyon", "Nice", "Paris", "A", "B", "C")
-question_8 = Countries("Italy", "Milan", "Naples", "Rome", "A", "B", "C")
-question_9 = Countries("Canada", "Montreal", "Ottawa", "Toronto", "A", "B", "C")
+question_4 = Countries("Japan", "Toyko", "Hiroshima", "Osaka", "A", "B", "C")
+question_5 = Countries("India", "New Deheli", "Chennai", "Mumbai", "A", "B", "C")
+question_6 = Countries("the United Kingdom", "London", "Birmingham", "Manchester", "A", "B", "C")
+question_7 = Countries("France", "Paris", "Lyon", "Nice", "A", "B", "C")
+question_8 = Countries("Italy", "Rome", "Milan", "Naples", "A", "B", "C")
+question_9 = Countries("Canada", "Ottawa", "Montreal", "Toronto", "A", "B", "C")
 question_10 = Countries("Brazil", "Brasilia", "Rio de Janerio", "Sao Paulo", "A", "B", "C")
 
 print()
@@ -115,10 +115,10 @@ def determine_if_b_is_the_corrrect_answer():
     print(f"You selected {answer}")
     print()
 
-    if answer.upper() == "A":
+    if answer.upper() == "B":
         print(f"Well done, {answer} is the correct answer")
         print()
-    elif answer.upper() == "B":
+    elif answer.upper() == "A":
         print(f"{answer} is not the correct answer")
         print()
     elif answer.upper() == "C":
@@ -130,3 +130,21 @@ def determine_if_b_is_the_corrrect_answer():
 determine_if_b_is_the_corrrect_answer()
 
 print()
+print(question_7.question_if_c_is_the_correct_answer())
+print()
+determine_if_c_is_the_corrrect_answer()
+
+print()
+print(question_8.question_if_c_is_the_correct_answer())
+print()
+determine_if_c_is_the_corrrect_answer()
+
+print()
+print(question_9.question_if_b_is_the_correct_answer())
+print()
+determine_if_b_is_the_corrrect_answer()
+
+print()
+print(question_10.question_if_a_is_the_correct_answer())
+print()
+determine_if_a_is_the_corrrect_answer()
